dff06466f205a7c72d354a202193be32f89b1168
Michael Molina <l.13molina@gmail.com>
2020-02-04 07:41:31 +0000

    Done.

--- countdown.rb
+++ countdown.rb
@@ -2,3 +2,15 @@
 
 def countdown
 end
+
+def countdown(number)
+  while number > 0
+  puts "#{number} SECOND(S)!"
+  number -=1 
+ end
+ "HAPPY NEW YEAR!"
+end
+
+def countdown_with_sleep(number)
+  sleep(5)
+end 
